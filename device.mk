ebdb3e85394074d14ad54e1f13369fdf7f368929
DarkAngelGR <asavvo01@gmail.com>
2019-05-20 14:51:37 +0000

    markw: add Sound Control in XiaomiParts
    • This is a port from the following commits made by @Hikari-no-Tenshi all credits go to him
    
    1st commit https://github.com/MiracleDROID-HnT/android_device_xiaomi_mido/commit/5ef2055cfb96bf6f101c25c174d3e7145b2c6116#diff-2032bcc5fa1da21685aea55638125e4e
    2nd commit https://github.com/MiracleDROID-HnT/android_device_xiaomi_mido/commit/6a5a2213c1f31dcffa2275ed62fe024d3ceaf6df#diff-2032bcc5fa1da21685aea55638125e4e

--- device.mk
+++ device.mk
@@ -550,10 +550,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# XiaomiParts
+# Extra Features
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    XiaomiParts \
+    GoogleCamera
 
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/markw/markw-vendor.mk)
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
+
